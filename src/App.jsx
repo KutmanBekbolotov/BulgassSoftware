499ddba964f119519942a101c3dbc40c24c47252
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-08-06 13:42:42 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//файл для настройки маршрутов страниц
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/homePage';
+
+function App(){
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
