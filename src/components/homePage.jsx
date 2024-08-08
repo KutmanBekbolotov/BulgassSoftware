ee8f50f636c17f717b11db0b3adc807997d5d463
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-08-08 10:10:41 +0000

    added new app

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -3,6 +3,7 @@ import "./homePage.css";
 import logo from "../assets/icons/logo.gif";
 import bulgass from "../assets/icons/bulgass.png"; 
 import react from "../assets/icons/react.webp";
+import note from "../assets/icons/note.jpg";
 import Sidebar from "./sidebar/sidebar";
 import WeatherWidget from "./widgets/weatherWidget/weatherWidget";
 
@@ -10,6 +11,7 @@ const services = [
     { name: 'Listen Up', icon: logo, link: 'https://listen-upp.netlify.app' },
     { name: 'BSW', icon: bulgass, link: 'https://bulgass-soft-works.netlify.app' },
     { name: 'Learn React', icon: react, link: 'https://legacy.reactjs.org/tutorial/tutorial.html'}, 
+    { name: 'Note', icon: note, link: 'https://onlinenotepad.org/notepad'},
 ];
 
 const Home = () => {
