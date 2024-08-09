78b5756f0f038d638d384610c1c464970f3ce235
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-08-09 20:54:20 +0000

    hompage redesign

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import "./homePage.css";
 import logo from "../assets/icons/logo.gif";
 import bulgass from "../assets/icons/bulgass.png"; 
-import react from "../assets/icons/react.webp";
+import react from "../assets/icons/react.png";
 import note from "../assets/icons/note.jpg";
 import Sidebar from "./sidebar/sidebar";
 import WeatherWidget from "./widgets/weatherWidget/weatherWidget";
 import Clock from "./widgets/clockWidget/clockWidget"
 import instagram1 from "../assets/icons/instagram1.webp";
+import telega from "../assets/icons/telega.png";
 
 const services = [
     { name: 'Listen Up', icon: logo, link: 'https://listen-upp.netlify.app' },
     { name: 'BSW', icon: bulgass, link: 'https://bulgass-soft-works.netlify.app' },
     { name: 'Learn React', icon: react, link: 'https://legacy.reactjs.org/tutorial/tutorial.html'}, 
     { name: 'Note', icon: note, link: 'https://onlinenotepad.org/notepad'},
-    {name: 'Instagram', icon: instagram1, link: 'https://www.instagram.com/bulgass.it/'},
+    { name: 'Instagram', icon: instagram1, link: 'https://www.instagram.com/bulgass.it/'},
+    { name: 'Tg group', icon: telega, link: 'https://t.me/+ejlSjQzju0UyZTQy'},
 ];
 
 const Home = () => (
@@ -23,7 +25,7 @@ const Home = () => (
             <Sidebar />
             <div className="header">
                 <img src={bulgass} alt="Header" className="header-image" />
-                <h1>Bulgass OS</h1>
+                <h1>Web OS</h1>
                 <pre>version: development</pre>
             </div>
             <div className="weather-container">
