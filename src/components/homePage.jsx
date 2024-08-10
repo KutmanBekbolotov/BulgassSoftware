2c6c0756db15f4e592272da262d3a0316b97fbaa
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-08-10 19:26:28 +0000

    Лег прод

--- src/components/homePage.jsx
+++ src/components/homePage.jsx
@@ -19,7 +19,7 @@ const services = [
     { name: 'Note', icon: note, link: 'https://onlinenotepad.org/notepad'},
     { name: 'Instagram', icon: instagram1, link: 'https://www.instagram.com/bulgass.it/'},
     { name: 'Tg group', icon: telega, link: 'https://t.me/+ejlSjQzju0UyZTQy'},
-    { name: 'GitGub', icon: github, link: 'https://github.com/'},
+    { name: 'GitGub', icon: GitGub, link: 'https://github.com/'},
     { name: 'ChatGPT', icon: chatgpt, link: 'https://chatgpt.com/'},
 ];
 
