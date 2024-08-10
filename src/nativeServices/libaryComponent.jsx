42e0b844506dbf0389260e6e947549a59eec0d57
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-08-10 20:31:56 +0000

    fixed book

--- src/nativeServices/libaryComponent.jsx
+++ src/nativeServices/libaryComponent.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import Sidebar from "../components/sidebar/sidebar";
 import Cbook from "../assets/bookCover/Cbook.png";
-import DockerDeepDive from "../assets/bookCover/DockerDeepDive.jpg"
-import reactBook from "../assets/bookCover/reactBook.jpg"
-import './libary.css'; 
-//sfd
+import DockerDeepDiveCover from "../assets/bookCover/DockerDeepDive.jpg"; // No need to import PDF
+import reactBook from "../assets/bookCover/reactBook.jpg";
+import './libary.css';
+
 const books = [
   {
     title: 'Learn C',
     cover: Cbook,
-    file: '/books/book1.pdf',
+    file: '/assets/books/learnC.pdf', // Correct path
   },
   {
     title: 'Docker Deep Dive',
-    cover: DockerDeepDive,
-    file: '/books/book2.pdf',
+    cover: DockerDeepDiveCover,
+    file: '/assets/books/DockerDeepDive.pdf', // Correct path
   },
   {
     title: 'React Book',
     cover: reactBook,
-    file: '/books/book3.pdf'
+    file: '/assets/books/reactBook.pdf',
   }
 ];
 
 const OnlineBooks = () => {
   return (
     <div className="books-container">
-         <Sidebar/>
+      <Sidebar/>
       {books.map((book, index) => (
         <div className="book-item" key={index}>
           <a href={book.file} target="_blank" rel="noopener noreferrer">
